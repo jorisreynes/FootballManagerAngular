a39cde3cd5a6ccda5193703fd2230c2cc8d3d368
Najib HARRIBOU <najib.harribou.ext@orange.com>
2023-01-24 16:21:30 +0100

    update formulaire

--- src/app/player.ts
+++ src/app/player.ts
@@ -1,8 +1,17 @@
 export class Player {
-    id: number;
-    age: number;
-    name: string;
-    picture: string;
-    type: string;
-    created: Date;
+  id: number;
+  age: number | undefined;
+  name: string;
+  picture: string;
+  type: string;
+  created: Date;
+
+  constructor(id: number, age: number, name: string, picture: string, type: string, created: Date) {
+    this.id = id;
+    this.age = age? age : undefined;
+    this.name = name;
+    this.picture = picture;
+    this.type = type;
+    this.created = created;
   }
+}
